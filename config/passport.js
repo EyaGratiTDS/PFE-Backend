940228aeeb8fe4feb0d622e2694b603537c2a93e
EyaGratiTDS <eya.grati@tds-technologies.fr>
2025-08-27 15:11:53 +0100

    Updates

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ const userController = require('../controllers/userController');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/auth/google/callback',
+  callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
   passReqToCallback: true,
   scope: ['profile', 'email'],
   prompt: 'consent'
